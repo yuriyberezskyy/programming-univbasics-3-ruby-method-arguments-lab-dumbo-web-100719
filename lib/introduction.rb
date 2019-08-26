6251321aa42fa0324da6ceb6f8e2a9e881ab2b92
Yuriy Berezskyy <bereza95@gmail.com>
2019-08-26 22:39:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,8 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-def introduction(name = "Josh")
-  puts "Hi,my name is #{name}."
+def introduction (name = "Josh")
+  puts "Hi, my name is #{name}."
 end
 
 
